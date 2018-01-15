c1f3d67d6cff55848f621d7ff6f1fb586e691bf4
newman <625236771@qq.com>
2018-01-15 13:21:54 +0800

    商品模块路由设计&创建组件结构

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,29 @@ import Login from '../component/login/Login.vue';
 // import Home from '../component/home/Home.vue';
 import Admin from '../component/admin/Admin.vue';
 
+import GoodsCtList from '../component/admin/goods/goods-content/GoodsCtList.vue';
+import GoodsCtEdit from '../component/admin/goods/goods-content/GoodsCtEdit.vue';
+import GoodsCgList from '../component/admin/goods/goods-category/GoodsCgList.vue';
+import GoodsCgEdit   from '../component/admin/goods/goods-category/GoodsCgEdit.vue';
+let adminChildren=[
+    // 商品模块 => 商品分类
+    { name: 'goodsCgList', path: 'goods/category/list', component: GoodsCgList },
+    { name: 'goodsCgEdit', path: 'goods/category/edit/:id', component: GoodsCgEdit },
+    { name: 'goodsCgAdd', path: 'goods/category/add', component: GoodsCgEdit },
+    // 商品模块 => 内容管理
+    { name: 'goodsCtList', path: 'goods/content/list', component: GoodsCtList },
+    { name: 'goodsCtEdit', path: 'goods/content/edit/:id', component: GoodsCtEdit },
+    { name: 'goodsCtAdd', path: 'goods/content/add', component: GoodsCtEdit },
+];
+
 export default{
     routes:[
         { name: 'login', path: '/login', component: Login },
         // { name: 'register', path: '/register', component: Register },
         // { name: 'home', path: '/home', component: Home },
         { name: 'admin', path: '/admin', component: Admin },
+        { path: '/', redirect: '/admin' },
+        { name: 'admin', path: '/admin', component: Admin,children:adminChildren },
+
     ]
 };
